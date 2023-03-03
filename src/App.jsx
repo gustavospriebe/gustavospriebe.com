d3e8fd3193f5de020d2154201a10cae85740ab19
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-03-03 19:44:50 -0300

    ga fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,17 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import { useState } from "react";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = import.meta.env.VITE_GOOGLE_ANALYTICS;
+ReactGA.initialize(TRACKING_ID);
+
+const SendAnalytics = () => {
+    ReactGA.send({
+        hitType: "pageview",
+        page: window.location.pathname,
+    });
+};
 
 function App() {
     const [language, setLanguage] = useState("pt-br");
