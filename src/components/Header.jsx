5c2d59b66648ea5d3bda51a69bea07f1ee084040
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-05-25 03:37:34 -0300

    tailwind header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,37 @@
 import logo from "../assets/logosvg.svg";
 import styles from "./Header.module.css";
 
+const links = ["Bio", "Projects"];
+
 function Header({ language, setLanguage }) {
     console.log(language);
     return (
-        <header className="mx-auto mb-28 flex w-3/4 items-center justify-between pt-7  ">
-            <img src={logo} alt="logo" className={styles.logo} />
-            <ul className={styles.links}>
-                <li>
-                    <a href="">Bio</a>
-                </li>
-                <li>
-                    <a href="">Projects</a>
-                </li>
+        <header className="mx-auto mb-28 flex w-3/4 items-center justify-between pt-14">
+            <img src={logo} alt="logo" className="h-7 w-20" />
+            <ul className="flex gap-8">
+                {links.map((l) => (
+                    <li className="text-link-inactive hover:brightness-90">
+                        <a href="">{l}</a>
+                    </li>
+                ))}
             </ul>
-            <ul className={styles.languages}>
+            <ul className="flex items-center justify-center text-sm gap-4">
                 <li
                     className={
-                        language === "en" ? styles.active : styles.inactive
+                        language === "en"
+                            ? "language-active"
+                            : "language-inactive"
                     }
                     onClick={() => setLanguage("en")}
                 >
                     ENG
                 </li>
-                <div className={styles.separator}></div>
+                <div className='separator'></div>
                 <li
                     className={
-                        language === "en" ? styles.inactive : styles.active
+                        language === "en"
+                            ? "language-inactive"
+                            : "language-active"
                     }
                     onClick={() => setLanguage("pt-br")}
                 >
